45116eb9b2c9ead217287134ffb8f945962744e9
Wiztech-01 <lynnnyaoke@gmail.com>
2023-12-04 10:05:40 +0300

    complete assignment

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,14 @@
 # your code goes here!
+
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+    
+    def match(self, l):
+        word_to_check = []
+        word_list = sorted([letter for letter in self.word])
+        
+        for anagram in l:
+            if word_list == sorted([letter for letter in anagram]):
+                word_to_check.append(anagram)
+        return word_to_check
